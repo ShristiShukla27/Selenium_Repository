741ebd1543d2d4a2370bc98190ffdbedada63dc6
shradhashukla <129949525+shradhashukla@users.noreply.github.com>
2025-08-08 11:35:20 +0530

    Added Sync Fork

---
+++ Automation_Framework/src/test/java/jewellery/Demo.java
@@ -0,0 +1,11 @@
+package jewellery;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void launch() {
+		Reporter.log("Learning Sync Fork",true);
+	}
+}
